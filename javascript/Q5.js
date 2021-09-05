de25b4c8d86c080aad38a8cf9f0e23006ba15db1
trilokregar <regar.trilok3@users.noreply.github.com>
2021-09-05 15:37:49 +0530

    Question5 posted

---
+++ javascript/Q5.js
@@ -0,0 +1,19 @@
+function distanceFromDirections(str) {
+    var totalDistance = 0;
+    var directionsWithValue = {
+        N: 1,
+        E: 1,
+        W: 1,
+        S: 1
+    }
+
+    for (var i = 0; i < str.length; i++) {
+        var elem = str[i];
+        if (elem === "N" || elem === "E" || elem === "W" || elem === "S") {
+            totalDistance += directionsWithValue[elem];
+        }
+    }
+    console.log("Total Distance = ", totalDistance);
+}
+distanceFromDirections("NNNNEEENE");
+distanceFromDirections("NEWS");
