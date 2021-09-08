bfae53ab3625b2849af237aecb0447f6ca5a7b54
trilokregar <regar.trilok3@users.noreply.github.com>
2021-09-08 16:01:24 +0530

    Q2,Q3,Q5 Solutions

--- javascript/Q3.js
+++ javascript/Q3.js
@@ -4,27 +4,22 @@ function divisible(start, end) {
     var div3 = [];
     var div5 = [];
 
-    // loop to read all element between given range //
     for (var i = start; i <= end; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             // push to the array//
             div3or5.push(i);
         }
         if (i % 3 == 0 && i % 5 == 0) {
-            // push to the array//
             div3and5.push(i);
         }
         if (i % 3 == 0) {
-            // push to the array//
             div3.push(i);
         }
         if (i % 5 == 0) {
-            // push to the array//
             div5.push(i);
         }
     }
     console.log(div3or5, "\n", div3and5, "\n", div3, "\n", div5);
 }
-// calling function at last //
 divisible(1, 10);
 divisible(3, 25);
